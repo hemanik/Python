8eb19941c1dc2f468cc69f7928f7adadc9cf7df5
hemanik <hemanikatyal@gmail.com>
2015-11-03 16:07:50 +0530

    Except Commit

--- datafinder.py
+++ datafinder.py
@@ -11,43 +11,58 @@ parser.add_argument('-ct', '--category', help="specify the category")
 parser.add_argument('-yr', '--year', help="specify the year")
 parser.add_argument('--datafile', nargs=2, help="specify two files")
 
-args = parser.parse_args()
+try:
+     args = parser.parse_args()
 
-if args.country and args.dataset and args.category :
-    print"COUNTRY:", args.country
-    with open('TB_notifications_2015-10-29.csv', 'rb') as file1, \
-         open('TB_data_dictionary_2015-10-29.csv', 'rb') as file2 :
+     if args.country and args.dataset and args.category :
 
-        reader1 = csv.DictReader(file1)
-        reader2 = csv.DictReader(file2)
+         with open('TB_notifications_2015-10-29.csv', 'rb') as file1, \
+              open('TB_data_dictionary_2015-10-29.csv', 'rb') as file2 :
+
+             reader1 = csv.DictReader(file1)
+             reader2 = csv.DictReader(file2)
        
-        value = "DNP"    
+             value = "DNP"    
+             
+             print"COUNTRY:", args.country
 
-        if args.year :
-              print"YEAR:", args.year
-      	  for row1 in reader1:
-                  if args.country in row1['iso2'] and args.year in row1['year']:
-                       value = row1[args.category]
-                       if value == '' or value == 0:
-                           value = "DNP"
-                       break   
+             if args.year :
+                  print"YEAR:", args.year
+      	          for row1 in reader1:
+                      if args.country in row1['iso2'] and args.year in row1['year']:
+                           value = row1[args.category]
+                           if value == '' or value == 0:
+                              value = "DNP"
+                           break   
        
-              for row2 in reader2:
-                  if args.category in row2['variable_name']:
-                       print "CATEGORY [", row2['definition'], "]:", value
-                       break
-        else :
-              for row2 in reader2:
-                  if args.category in row2['variable_name']:
-                       print "CATEGORY [", row2['definition'], "]"
-                       break
+                  for row2 in reader2:
+                      if args.category in row2['variable_name']:
+                           print "CATEGORY [", row2['definition'], "]:", value
+                           break
+             else :
+                  for row2 in reader2:
+                      if args.category in row2['variable_name']:
+                           print "CATEGORY [", row2['definition'], "]"
+                           break
          
-              for row1 in reader1:
-                  if args.country in row1['iso2']:
-                       value = row1[args.category]
-                       year  = row1['year'] 
-                       if value == '' or value == 0:
-                          value = "DNP"   
-                       print year, ":", value
-else :
-     print "Invalid Arguments. Check help."  
+                  for row1 in reader1:
+                      if args.country in row1['iso2']:
+                          value = row1[args.category]
+                          year  = row1['year'] 
+                          if value == '' or value == 0:
+                             value = "DNP"   
+
+                          print year, ":", value
+
+     else:
+          print "Invalid Arguments. Check help."
+                                           
+except argparse.ArgumentError, exc:
+     print exc.message
+
+except IOError:  
+     print "Cannot open input files."
+
+except Exception, e:
+     print ("Error: %s" %e)  
+
